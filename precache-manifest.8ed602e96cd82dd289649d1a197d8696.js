75b96719a88623cdfe18213d9f5db8d6b89e4d67
Volodymyr Ivanuts <Ivanyts777@gmail.com>
2020-09-10 22:53:11 +0300

    Updates

---
+++ precache-manifest.8ed602e96cd82dd289649d1a197d8696.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2adafbe22f5f566e640562ddd4f8199",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "d342e8420c5b58a80ccf",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.b8621a92.chunk.css"
+  },
+  {
+    "revision": "e16b5789154e8985d7d6",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.94f11521.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.94f11521.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d342e8420c5b58a80ccf",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.e1bc2329.chunk.js"
+  },
+  {
+    "revision": "00ed64b4697a879a9e95",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.4e03020b.js"
+  }
+]);
